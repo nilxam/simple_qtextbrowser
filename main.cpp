36d07da97431d628c0d8a434b49a8bb7783a146b
Max Lin <mlin@suse.com>
2018-07-31 00:26:50 +0800

    Add another browser showing Html List

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QHBoxLayout>
 #include <QTextBrowser>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -8,10 +9,15 @@ int main(int argc, char *argv[])
     QWidget *window = new QWidget;
     QHBoxLayout *layout = new QHBoxLayout;
     QTextBrowser *browser = new QTextBrowser;
+    QTextBrowser *browser2 = new QTextBrowser;
     layout->addWidget(browser);
+    layout->addWidget(browser2);
 
-    QString text = "abcde: edcba";
+    QString text = "Plain text\nTest";
     browser->setPlainText(text);
+    QString html = "<h4>Html List</h4><ul><li>line1</li></ul>";
+    browser2->setHtml(html);
+    qInfo() << browser2->toPlainText();
 
     window->setLayout(layout);
     window->show();
